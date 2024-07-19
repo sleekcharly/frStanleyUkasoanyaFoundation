66af0f773047f73526e9f7260adc6059276c0348
sleekcharly <c_ukasoanya@yahoo.com>
2024-07-19 15:00:07 +0100

    created the clauses section of the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,9 +3,8 @@ import Hero from '/public/images/hero.webp';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import { MoveDown } from 'lucide-react';
-import PriestFormation from '/public/images/priest-formation.webp';
-import FamilyLife from '/public/images/family-life.webp';
-import { BsExclamationCircle } from 'react-icons/bs';
+import MiniAbout from '@/components/home/MiniAbout';
+import Causes from '@/components/home/Causes';
 
 type Props = {};
 
@@ -38,57 +37,11 @@ const Home = (props: Props) => {
         </header>
       </div>
 
-      <section className="bg-white flex flex-col lg:flex-row items-center justify-center gap-6 p-auto text-center md:text-left p-10 lg:px-32 xl:px-32 xl:py-32">
-        <div className="flex-1 max-w-[700px]">
-          <h2 className="uppercase text-[#bd873c] tracking-wider font-bold text-lg mb-3">
-            Who we are
-          </h2>
-          <h3 className="text-2xl lg:text-3xl font-semibold mb-3">
-            Promoting Priestly Vocations and Family Values
-          </h3>
+      {/* Mini About section */}
+      <MiniAbout />
 
-          <hr className="bg-[#bd873c] h-1 w-full md:w-48 mb-10" />
-
-          <p className="tracking-wide text-lg">
-            Welcome to the Rev. Fr. Stanley Ukasoanya Foundation! Our mission is
-            to inspire priestly and religious vocations, nurture Catholic family
-            values, and uplift the less privileged. We empower communities
-            through education, livelihood promotion, counseling, and relief
-            initiatives. Dedicated to making a meaningful impact, we are
-            committed to transforming lives with our unwavering support.
-          </p>
-
-          <div className="mt-3 flex flex-col gap-2 items-center md:items-start">
-            <div className="flex items-center space-x-2">
-              <BsExclamationCircle className="w-5 h-5 text-[#bd873c]" />
-              <Link href="/about" className="text-[#bd873c] text-xs">
-                Learn more about us
-              </Link>
-            </div>
-            <hr className="bg-[#bd873c] h-0.5 w-48" />
-          </div>
-        </div>
-        <div className="relative">
-          <div className="relative w-[300px] h-[250px] md:w-[400px] md:h-[350px] shadow-xl">
-            <Image
-              src={PriestFormation}
-              fill
-              alt="Priest Formation"
-              className="object-cover rounded-md"
-            />
-          </div>
-          <div className="absolute -bottom-5 -right-10 md:-bottom-10 md:-right-20">
-            <div className="relative w-[220px] h-[140px] md:w-[280px] md:h-[200px] shadow-xl">
-              <Image
-                src={FamilyLife}
-                fill
-                alt="Priest Formation"
-                className="object-cover rounded-md"
-              />
-            </div>
-          </div>
-        </div>
-      </section>
+      {/* Causes Section */}
+      <Causes />
     </div>
   );
 };
---
+++ components/home/Causes.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { Button } from '../ui/button';
+import Image from 'next/image';
+import PriestFormation from '/public/images/priest-formation2.webp';
+import FamilyLife from '/public/images/family-life2.webp';
+import Empowerment from '/public/images/empowerment.webp';
+function Causes() {
+  return (
+    <section className="bg-gray-200 p-10 lg:px-20">
+      <div className="flex flex-col md:flex-row items-center  gap-6 mb-14">
+        <p className="font-semibold tracking-widest text-gray-500">
+          IMPACT AREAS
+        </p>
+        <div className="bg-[#bd873c] h-0.5 w-40" />
+        <h2 className="font-sans text-2xl md:text-3xl lg:text-4xl font-bold">
+          Our Causes
+        </h2>
+      </div>
+
+      {/* grid area */}
+      <div className="flex flex-col items-center justify-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="max-w-[450px] border border-gray-300 rounded-lg shadow-md">
+            <div className="relative w-full max-w-[450px] h-[250px]">
+              <Image
+                src={PriestFormation}
+                alt="Priestly formation"
+                fill
+                className="object-cover rounded-t-lg"
+              />
+            </div>
+            <div className="p-4 ">
+              <h3 className="text-xl font-semibold mb-4">Priestly Formation</h3>
+              <p className="mb-8">
+                Inspiring Future Leaders: Championing Priestly and Religious
+                Vocations.
+              </p>
+              <hr className="mb-4 h-1 bg-[#bd873c]" />
+              <Button>Learn more</Button>
+            </div>
+          </div>
+          <div className="max-w-[450px] border border-gray-300 rounded-lg shadow-md">
+            <div className="relative  w-full max-w-[450px] h-[250px]">
+              <Image
+                src={FamilyLife}
+                alt="Family life photo"
+                fill
+                className="object-cover rounded-t-lg"
+              />
+            </div>
+            <div className="p-4">
+              <h3 className="text-xl font-semibold mb-4">Family Life</h3>
+              <p className="mb-8">
+                Fostering Family Values for a Stronger Bond and Brighter Future.
+              </p>
+              <hr className="mb-4 h-1 bg-[#bd873c]" />
+              <Button>Learn more</Button>
+            </div>
+          </div>
+          <div className="max-w-[450px] border border-gray-300 rounded-lg shadow-md">
+            <div className="relative  w-full max-w-[450px] h-[250px]">
+              <Image
+                src={Empowerment}
+                alt="Empowerment photo"
+                fill
+                className="object-cover rounded-t-lg"
+              />
+            </div>
+            <div className="p-4">
+              <h3 className="text-xl font-semibold mb-4">Empowerment</h3>
+              <p className="mb-8">
+                Transforming lives: Education, livelihoods, counseling, and
+                relief for the less privileged.
+              </p>
+              <hr className="mb-4 h-1 bg-[#bd873c]" />
+              <Button>Learn more</Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Causes;
